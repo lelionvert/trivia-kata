e7be1f27d61517a115f7aee2615f0c8c1e8ad335
Axel Ollivier <axel.ollivier@lacombedulionvert.fr>
2020-03-27 11:39:11 +0100

    test of no winner when roll 3

--- java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
@@ -84,7 +84,7 @@ public class SomeTest {
 	}
 
 	@Test
-	public void deux() {
+	public void testThatTheGameReturnsTheSameResultWhenHavingThreePlayersAndRollingTwo() {
 		int randomRoll = 2;
 		PrintStream originalOut = System.out;
 		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
@@ -100,6 +100,41 @@ public class SomeTest {
 				"Chet's new location is 2\r\n" +
 				"The category is Sports\r\n" +
 				"Sports Question 0\r\n" +
+				"Question was incorrectly answered\r\n" +
+				"Chet was sent to the penalty box\r\n";
+
+		Game aGame = new Game();
+		aGame.add("Chet");
+		aGame.add("Pat");
+		aGame.add("Sue");
+
+
+		aGame.roll(randomRoll);
+
+		boolean notAWinner = aGame.wrongAnswer();
+
+		assertTrue(notAWinner);
+		assertEquals(expectedOut, outContent.toString());
+		System.setOut(originalOut);
+	}
+
+	@Test
+	public void testThatTheGameReturnsTheSameResultWhenHavingThreePlayersAndRollingThree() {
+		int randomRoll = 3;
+		PrintStream originalOut = System.out;
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+		String expectedOut = "Chet was added\r\n" +
+				"They are player number 1\r\n" +
+				"Pat was added\r\n" +
+				"They are player number 2\r\n" +
+				"Sue was added\r\n" +
+				"They are player number 3\r\n" +
+				"Chet is the current player\r\n" +
+				"They have rolled a 3\r\n" +
+				"Chet's new location is 3\r\n" +
+				"The category is Rock\r\n" +
+				"Rock Question 0\r\n" +
 				"Question was incorrectly answered\r\n" +
 				"Chet was sent to the penalty box\r\n";
 

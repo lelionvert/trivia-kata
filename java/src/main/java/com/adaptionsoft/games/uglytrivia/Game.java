f5545bb34368dafed676a04da48e8c7431e436b8
Axel Ollivier <axel.ollivier@lacombedulionvert.fr>
2020-03-27 15:11:30 +0100

    continue refactoring current category

--- java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -101,18 +101,13 @@ public class Game {
 	
 	private String currentCategory() {
 
-		switch (places[currentPlayer]) {
+		switch (places[currentPlayer] % 4) {
+
 			case 0:
-			case 4:
-			case 8:
 				return "Pop";
 			case 1:
-			case 5:
-			case 9:
 				return "Science";
 			case 2:
-			case 6:
-			case 10:
 				return "Sports";
 			default:
 				return "Rock";

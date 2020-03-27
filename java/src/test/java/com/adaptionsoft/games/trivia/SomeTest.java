ad1b4b2f3862aedb8438bce97cf590e27985e706
Axel Ollivier <axel.ollivier@lacombedulionvert.fr>
2020-03-27 12:08:19 +0100

    Tests are now parametrized

--- java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
@@ -1,9 +1,13 @@
 package com.adaptionsoft.games.trivia;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.adaptionsoft.games.uglytrivia.Game;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -38,14 +42,14 @@ public class SomeTest {
 		aGame.roll(4);
 		boolean notAWinner = aGame.wrongAnswer();
 
-		assertTrue(notAWinner);
-		assertEquals(expectedOut, outContent.toString());
+		assertThat(notAWinner).isTrue();
+		assertThat(expectedOut).isEqualTo(outContent.toString());
 		System.setOut(originalOut);
 	}
 
-	@Test
-	public void testThatTheGameReturnsTheSameResultWhenHavingThreePlayersAndRollingOne() {
-		int randomRoll = 1;
+	@ParameterizedTest
+	@CsvSource({"1, Science","2, Sports", "3, Rock", "4, Pop", "5, Science", "6, Sports"})
+	public void testParam(int randomRoll, String category) {
 		PrintStream originalOut = System.out;
 		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 		System.setOut(new PrintStream(outContent));
@@ -56,10 +60,10 @@ public class SomeTest {
 				"Sue was added\r\n" +
 				"They are player number 3\r\n" +
 				"Chet is the current player\r\n" +
-				"They have rolled a 1\r\n" +
-				"Chet's new location is 1\r\n" +
-				"The category is Science\r\n" +
-				"Science Question 0\r\n" +
+				"They have rolled a " + randomRoll + "\r\n" +
+				"Chet's new location is " + randomRoll + "\r\n" +
+				"The category is " + category + "\r\n" +
+				category + " Question 0\r\n" +
 				"Question was incorrectly answered\r\n" +
 				"Chet was sent to the penalty box\r\n";
 
@@ -73,183 +77,9 @@ public class SomeTest {
 
 		boolean notAWinner = aGame.wrongAnswer();
 
-		assertTrue(notAWinner);
-		assertEquals(expectedOut, outContent.toString());
+		assertThat(notAWinner).isTrue();
+		assertThat(expectedOut).isEqualTo(outContent.toString());
 		System.setOut(originalOut);
 	}
 
-	@Test
-	public void testThatTheGameReturnsTheSameResultWhenHavingThreePlayersAndRollingTwo() {
-		int randomRoll = 2;
-		PrintStream originalOut = System.out;
-		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-		System.setOut(new PrintStream(outContent));
-		String expectedOut = "Chet was added\r\n" +
-				"They are player number 1\r\n" +
-				"Pat was added\r\n" +
-				"They are player number 2\r\n" +
-				"Sue was added\r\n" +
-				"They are player number 3\r\n" +
-				"Chet is the current player\r\n" +
-				"They have rolled a 2\r\n" +
-				"Chet's new location is 2\r\n" +
-				"The category is Sports\r\n" +
-				"Sports Question 0\r\n" +
-				"Question was incorrectly answered\r\n" +
-				"Chet was sent to the penalty box\r\n";
-
-		Game aGame = new Game();
-		aGame.add("Chet");
-		aGame.add("Pat");
-		aGame.add("Sue");
-
-
-		aGame.roll(randomRoll);
-
-		boolean notAWinner = aGame.wrongAnswer();
-
-		assertTrue(notAWinner);
-		assertEquals(expectedOut, outContent.toString());
-		System.setOut(originalOut);
-	}
-
-	@Test
-	public void testThatTheGameReturnsTheSameResultWhenHavingThreePlayersAndRollingThree() {
-		int randomRoll = 3;
-		PrintStream originalOut = System.out;
-		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-		System.setOut(new PrintStream(outContent));
-		String expectedOut = "Chet was added\r\n" +
-				"They are player number 1\r\n" +
-				"Pat was added\r\n" +
-				"They are player number 2\r\n" +
-				"Sue was added\r\n" +
-				"They are player number 3\r\n" +
-				"Chet is the current player\r\n" +
-				"They have rolled a 3\r\n" +
-				"Chet's new location is 3\r\n" +
-				"The category is Rock\r\n" +
-				"Rock Question 0\r\n" +
-				"Question was incorrectly answered\r\n" +
-				"Chet was sent to the penalty box\r\n";
-
-		Game aGame = new Game();
-		aGame.add("Chet");
-		aGame.add("Pat");
-		aGame.add("Sue");
-
-
-		aGame.roll(randomRoll);
-
-		boolean notAWinner = aGame.wrongAnswer();
-
-		assertTrue(notAWinner);
-		assertEquals(expectedOut, outContent.toString());
-		System.setOut(originalOut);
-	}
-
-	@Test
-	public void testThatTheGameReturnsTheSameResultWhenHavingThreePlayersAndRollingFour() {
-		int randomRoll = 4;
-		PrintStream originalOut = System.out;
-		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-		System.setOut(new PrintStream(outContent));
-		String expectedOut = "Chet was added\r\n" +
-				"They are player number 1\r\n" +
-				"Pat was added\r\n" +
-				"They are player number 2\r\n" +
-				"Sue was added\r\n" +
-				"They are player number 3\r\n" +
-				"Chet is the current player\r\n" +
-				"They have rolled a 4\r\n" +
-				"Chet's new location is 4\r\n" +
-				"The category is Pop\r\n" +
-				"Pop Question 0\r\n" +
-				"Question was incorrectly answered\r\n" +
-				"Chet was sent to the penalty box\r\n";
-
-		Game aGame = new Game();
-		aGame.add("Chet");
-		aGame.add("Pat");
-		aGame.add("Sue");
-
-
-		aGame.roll(randomRoll);
-
-		boolean notAWinner = aGame.wrongAnswer();
-
-		assertTrue(notAWinner);
-		assertEquals(expectedOut, outContent.toString());
-		System.setOut(originalOut);
-	}
-
-	@Test
-	public void testThatTheGameReturnsTheSameResultWhenHavingThreePlayersAndRollingFive() {
-		int randomRoll = 5;
-		PrintStream originalOut = System.out;
-		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-		System.setOut(new PrintStream(outContent));
-		String expectedOut = "Chet was added\r\n" +
-				"They are player number 1\r\n" +
-				"Pat was added\r\n" +
-				"They are player number 2\r\n" +
-				"Sue was added\r\n" +
-				"They are player number 3\r\n" +
-				"Chet is the current player\r\n" +
-				"They have rolled a 5\r\n" +
-				"Chet's new location is 5\r\n" +
-				"The category is Science\r\n" +
-				"Science Question 0\r\n" +
-				"Question was incorrectly answered\r\n" +
-				"Chet was sent to the penalty box\r\n";
-
-		Game aGame = new Game();
-		aGame.add("Chet");
-		aGame.add("Pat");
-		aGame.add("Sue");
-
-
-		aGame.roll(randomRoll);
-
-		boolean notAWinner = aGame.wrongAnswer();
-
-		assertTrue(notAWinner);
-		assertEquals(expectedOut, outContent.toString());
-		System.setOut(originalOut);
-	}
-
-	@Test
-	public void testThatTheGameReturnsTheSameResultWhenHavingThreePlayersAndRollingSix() {
-		int randomRoll = 6;
-		PrintStream originalOut = System.out;
-		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-		System.setOut(new PrintStream(outContent));
-		String expectedOut = "Chet was added\r\n" +
-				"They are player number 1\r\n" +
-				"Pat was added\r\n" +
-				"They are player number 2\r\n" +
-				"Sue was added\r\n" +
-				"They are player number 3\r\n" +
-				"Chet is the current player\r\n" +
-				"They have rolled a 6\r\n" +
-				"Chet's new location is 6\r\n" +
-				"The category is Sports\r\n" +
-				"Sports Question 0\r\n" +
-				"Question was incorrectly answered\r\n" +
-				"Chet was sent to the penalty box\r\n";
-
-		Game aGame = new Game();
-		aGame.add("Chet");
-		aGame.add("Pat");
-		aGame.add("Sue");
-
-
-		aGame.roll(randomRoll);
-
-		boolean notAWinner = aGame.wrongAnswer();
-
-		assertTrue(notAWinner);
-		assertEquals(expectedOut, outContent.toString());
-		System.setOut(originalOut);
-	}
 }

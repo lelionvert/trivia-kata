c3f4359230f00aac8d16865c2157d622b52cdae5
Axel Ollivier <axel.ollivier@lacombedulionvert.fr>
2020-03-27 11:44:15 +0100

    test of no winner when roll 5

--- java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
@@ -182,4 +182,39 @@ public class SomeTest {
 		assertEquals(expectedOut, outContent.toString());
 		System.setOut(originalOut);
 	}
+
+	@Test
+	public void testThatTheGameReturnsTheSameResultWhenHavingThreePlayersAndRollingFive() {
+		int randomRoll = 5;
+		PrintStream originalOut = System.out;
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+		String expectedOut = "Chet was added\r\n" +
+				"They are player number 1\r\n" +
+				"Pat was added\r\n" +
+				"They are player number 2\r\n" +
+				"Sue was added\r\n" +
+				"They are player number 3\r\n" +
+				"Chet is the current player\r\n" +
+				"They have rolled a 5\r\n" +
+				"Chet's new location is 5\r\n" +
+				"The category is Science\r\n" +
+				"Science Question 0\r\n" +
+				"Question was incorrectly answered\r\n" +
+				"Chet was sent to the penalty box\r\n";
+
+		Game aGame = new Game();
+		aGame.add("Chet");
+		aGame.add("Pat");
+		aGame.add("Sue");
+
+
+		aGame.roll(randomRoll);
+
+		boolean notAWinner = aGame.wrongAnswer();
+
+		assertTrue(notAWinner);
+		assertEquals(expectedOut, outContent.toString());
+		System.setOut(originalOut);
+	}
 }
